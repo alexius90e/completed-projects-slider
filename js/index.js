350cb9f0cc9d1d2c62248ee1e033d593da94b2a5
Aliaksei Karzhou <aliaksei_karzhou1@epam.com>
2023-10-16 01:24:48 +0300

    refactor: refatored logic of slider

--- js/index.js
+++ js/index.js
@@ -1,35 +1,67 @@
-const nav = document.querySelector('.slider__nav');
-const navItems = document.querySelectorAll('.slider__nav-btn');
-const sliderPages = document.querySelectorAll('.slider__page');
+const hotels = [
+  {
+    id: 1,
+    name: 'LCD admiral',
+    city: 'Rostov-on-Don',
+    apartmentArea: '81 m2',
+    repairTime: '3.5 months',
+    repairCost: 'Upon request',
+  },
+  {
+    id: 2,
+    name: 'Thieves',
+    city: 'Sochi',
+    apartmentArea: '105 m2',
+    repairTime: '4 months',
+    repairCost: 'Upon request',
+  },
+  {
+    id: 3,
+    name: 'Patriotic',
+    city: 'Rostov-on-Don',
+    apartmentArea: '93 m2',
+    repairTime: '3 months',
+    repairCost: 'Upon request',
+  },
+];
 
-nav.addEventListener('click', (event) => {
-  const isNavItem = event.target.classList.contains('slider__nav-btn');
-  navItems.forEach((item) => item.classList.remove('active'));
-  event.target.classList.add('active');
+const cityWithNameElem = document.getElementById('cityWithName');
+const apartmentAreaElem = document.getElementById('apartmentArea');
+const repairTimeElem = document.getElementById('repairTime');
+const repairCostElem = document.getElementById('repairCost');
 
-  if (isNavItem) {
-    const activePageId = event.target.dataset.page;
+const sliderNavButtons = document.querySelectorAll('.slider__nav-btn');
 
-    sliderPages.forEach((page) => {
-      if (page.id === activePageId) page.classList.add('active');
-      if (page.id !== activePageId) page.classList.remove('active');
-    });
-  }
+const baseHotel = hotels.rostovOnDonAdmiral;
+
+const slider = new Swiper('#sliderMain', {
+  loop: true,
+  pagination: {
+    el: '.slider__controls-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.slider__controls-next',
+    prevEl: '.slider__controls-prev',
+  },
 });
 
-const sliderIds = ['admiralRostovOnDon', 'thievesSochi', 'patrioticRostovOnDon'];
+slider.on('slideChange', function () {
+  const activeSlideIndex = slider.realIndex;
+  const activeHotel = hotels[slider.realIndex];
+  sliderNavButtons.forEach((button, index) => {
+    if (index === activeSlideIndex) button.classList.add('active');
+    if (index !== activeSlideIndex) button.classList.remove('active');
+  });
 
-const sliders = sliderIds.map((id) => {
-  return new Swiper(`#${id}Swiper`, {
-    loop: true,
+  cityWithNameElem.innerHTML = `<span>${activeHotel.city}</span><span>${activeHotel.name}</span>`;
+  apartmentAreaElem.innerText = activeHotel.apartmentArea;
+  repairTimeElem.innerText = activeHotel.repairTime;
+  repairCostElem.innerText = activeHotel.repairCost;
+});
 
-    pagination: {
-      el: `#${id} .slider__controls-pagination`,
-      clickable: true,
-    },
-    navigation: {
-      nextEl: `#${id} .slider__controls-next`,
-      prevEl: `#${id} .slider__controls-prev`,
-    },
+sliderNavButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    slider.slideToLoop(index);
   });
 });
